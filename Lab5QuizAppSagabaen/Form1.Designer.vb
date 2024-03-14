33a83595a42f146908b48dbf35d143ada4913d7a
Dre-OS <hlsagabaen@gmail.com>
2024-03-14 16:04:45 +0800

    Trying Swap

--- Lab5QuizAppSagabaen/Form1.Designer.vb
+++ Lab5QuizAppSagabaen/Form1.Designer.vb
@@ -23,161 +23,190 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.pbx_Banana = New System.Windows.Forms.PictureBox()
-        Me.pbx_Orange = New System.Windows.Forms.PictureBox()
-        Me.pbx_DragonFruit = New System.Windows.Forms.PictureBox()
         Me.btn_Check = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.pbx_AnswerOrange = New System.Windows.Forms.PictureBox()
-        Me.pbx_AnswerBanana = New System.Windows.Forms.PictureBox()
-        Me.pbx_AnswerDragonFruit = New System.Windows.Forms.PictureBox()
+        Me.lbl_ScoreNum = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lbl_Reset = New System.Windows.Forms.Label()
+        Me.pbx_Trash = New System.Windows.Forms.PictureBox()
         Me.lbl_Orange = New System.Windows.Forms.Label()
         Me.lbl_DragonFruit = New System.Windows.Forms.Label()
         Me.lbl_Banana = New System.Windows.Forms.Label()
-        CType(Me.pbx_Banana, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.pbx_Orange, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.pbx_DragonFruit, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.pbx_AnswerOrange, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.pbx_AnswerBanana, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.pbx_AnswerDragonFruit, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pbx_Question3 = New System.Windows.Forms.PictureBox()
+        Me.pbx_Question1 = New System.Windows.Forms.PictureBox()
+        Me.pbx_Question2 = New System.Windows.Forms.PictureBox()
+        Me.pbx_Answer3 = New System.Windows.Forms.PictureBox()
+        Me.pbx_Answer1 = New System.Windows.Forms.PictureBox()
+        Me.pbx_Answer2 = New System.Windows.Forms.PictureBox()
         Me.Panel1.SuspendLayout()
+        CType(Me.pbx_Trash, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbx_Question3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbx_Question1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbx_Question2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbx_Answer3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbx_Answer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbx_Answer2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'pbx_Banana
-        '
-        Me.pbx_Banana.Image = CType(resources.GetObject("pbx_Banana.Image"), System.Drawing.Image)
-        Me.pbx_Banana.InitialImage = CType(resources.GetObject("pbx_Banana.InitialImage"), System.Drawing.Image)
-        Me.pbx_Banana.Location = New System.Drawing.Point(139, 401)
-        Me.pbx_Banana.Name = "pbx_Banana"
-        Me.pbx_Banana.Size = New System.Drawing.Size(110, 110)
-        Me.pbx_Banana.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.pbx_Banana.TabIndex = 0
-        Me.pbx_Banana.TabStop = False
-        '
-        'pbx_Orange
-        '
-        Me.pbx_Orange.Image = Global.Lab5QuizAppSagabaen.My.Resources.Resources.Orange
-        Me.pbx_Orange.InitialImage = CType(resources.GetObject("pbx_Orange.InitialImage"), System.Drawing.Image)
-        Me.pbx_Orange.Location = New System.Drawing.Point(23, 401)
-        Me.pbx_Orange.Name = "pbx_Orange"
-        Me.pbx_Orange.Size = New System.Drawing.Size(110, 110)
-        Me.pbx_Orange.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.pbx_Orange.TabIndex = 1
-        Me.pbx_Orange.TabStop = False
-        '
-        'pbx_DragonFruit
-        '
-        Me.pbx_DragonFruit.Image = CType(resources.GetObject("pbx_DragonFruit.Image"), System.Drawing.Image)
-        Me.pbx_DragonFruit.InitialImage = CType(resources.GetObject("pbx_DragonFruit.InitialImage"), System.Drawing.Image)
-        Me.pbx_DragonFruit.Location = New System.Drawing.Point(255, 401)
-        Me.pbx_DragonFruit.Name = "pbx_DragonFruit"
-        Me.pbx_DragonFruit.Size = New System.Drawing.Size(110, 110)
-        Me.pbx_DragonFruit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.pbx_DragonFruit.TabIndex = 2
-        Me.pbx_DragonFruit.TabStop = False
         '
         'btn_Check
         '
-        Me.btn_Check.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Check.BackColor = System.Drawing.Color.Gold
+        Me.btn_Check.FlatAppearance.BorderSize = 0
+        Me.btn_Check.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Check.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Check.Location = New System.Drawing.Point(390, 402)
         Me.btn_Check.Name = "btn_Check"
         Me.btn_Check.Size = New System.Drawing.Size(101, 42)
         Me.btn_Check.TabIndex = 3
         Me.btn_Check.Text = "Check"
-        Me.btn_Check.UseVisualStyleBackColor = True
+        Me.btn_Check.UseVisualStyleBackColor = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(398, 466)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(93, 31)
+        Me.Label1.Size = New System.Drawing.Size(100, 38)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Score:"
         '
-        'Label2
+        'lbl_ScoreNum
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(529, 424)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(68, 73)
-        Me.Label2.TabIndex = 5
-        Me.Label2.Text = "0"
-        '
-        'pbx_AnswerOrange
-        '
-        Me.pbx_AnswerOrange.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.pbx_AnswerOrange.Location = New System.Drawing.Point(408, 24)
-        Me.pbx_AnswerOrange.Name = "pbx_AnswerOrange"
-        Me.pbx_AnswerOrange.Size = New System.Drawing.Size(110, 110)
-        Me.pbx_AnswerOrange.TabIndex = 8
-        Me.pbx_AnswerOrange.TabStop = False
-        '
-        'pbx_AnswerBanana
-        '
-        Me.pbx_AnswerBanana.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.pbx_AnswerBanana.Location = New System.Drawing.Point(117, 24)
-        Me.pbx_AnswerBanana.Name = "pbx_AnswerBanana"
-        Me.pbx_AnswerBanana.Size = New System.Drawing.Size(110, 110)
-        Me.pbx_AnswerBanana.TabIndex = 7
-        Me.pbx_AnswerBanana.TabStop = False
-        '
-        'pbx_AnswerDragonFruit
-        '
-        Me.pbx_AnswerDragonFruit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.pbx_AnswerDragonFruit.Location = New System.Drawing.Point(255, 218)
-        Me.pbx_AnswerDragonFruit.Name = "pbx_AnswerDragonFruit"
-        Me.pbx_AnswerDragonFruit.Size = New System.Drawing.Size(110, 110)
-        Me.pbx_AnswerDragonFruit.TabIndex = 6
-        Me.pbx_AnswerDragonFruit.TabStop = False
+        Me.lbl_ScoreNum.AutoSize = True
+        Me.lbl_ScoreNum.Font = New System.Drawing.Font("Comic Sans MS", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_ScoreNum.Location = New System.Drawing.Point(529, 424)
+        Me.lbl_ScoreNum.Name = "lbl_ScoreNum"
+        Me.lbl_ScoreNum.Size = New System.Drawing.Size(77, 90)
+        Me.lbl_ScoreNum.TabIndex = 5
+        Me.lbl_ScoreNum.Text = "0"
         '
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.lbl_Reset)
+        Me.Panel1.Controls.Add(Me.pbx_Trash)
         Me.Panel1.Controls.Add(Me.lbl_Orange)
         Me.Panel1.Controls.Add(Me.lbl_DragonFruit)
         Me.Panel1.Controls.Add(Me.lbl_Banana)
-        Me.Panel1.Controls.Add(Me.pbx_AnswerOrange)
-        Me.Panel1.Controls.Add(Me.pbx_AnswerBanana)
-        Me.Panel1.Controls.Add(Me.pbx_AnswerDragonFruit)
+        Me.Panel1.Controls.Add(Me.pbx_Question3)
+        Me.Panel1.Controls.Add(Me.pbx_Question1)
+        Me.Panel1.Controls.Add(Me.pbx_Question2)
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(640, 387)
         Me.Panel1.TabIndex = 9
         '
+        'lbl_Reset
+        '
+        Me.lbl_Reset.AutoSize = True
+        Me.lbl_Reset.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Reset.Location = New System.Drawing.Point(520, 218)
+        Me.lbl_Reset.Name = "lbl_Reset"
+        Me.lbl_Reset.Size = New System.Drawing.Size(60, 23)
+        Me.lbl_Reset.TabIndex = 14
+        Me.lbl_Reset.Text = "Reset?"
+        Me.lbl_Reset.Visible = False
+        '
+        'pbx_Trash
+        '
+        Me.pbx_Trash.Image = CType(resources.GetObject("pbx_Trash.Image"), System.Drawing.Image)
+        Me.pbx_Trash.Location = New System.Drawing.Point(501, 242)
+        Me.pbx_Trash.Name = "pbx_Trash"
+        Me.pbx_Trash.Size = New System.Drawing.Size(96, 120)
+        Me.pbx_Trash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pbx_Trash.TabIndex = 13
+        Me.pbx_Trash.TabStop = False
+        '
         'lbl_Orange
         '
         Me.lbl_Orange.AutoSize = True
-        Me.lbl_Orange.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
-        Me.lbl_Orange.Location = New System.Drawing.Point(420, 137)
+        Me.lbl_Orange.Font = New System.Drawing.Font("Comic Sans MS", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Orange.Location = New System.Drawing.Point(384, 137)
         Me.lbl_Orange.Name = "lbl_Orange"
-        Me.lbl_Orange.Size = New System.Drawing.Size(86, 31)
+        Me.lbl_Orange.Size = New System.Drawing.Size(89, 38)
         Me.lbl_Orange.TabIndex = 12
         Me.lbl_Orange.Text = "Name"
         '
         'lbl_DragonFruit
         '
         Me.lbl_DragonFruit.AutoSize = True
-        Me.lbl_DragonFruit.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
-        Me.lbl_DragonFruit.Location = New System.Drawing.Point(224, 331)
+        Me.lbl_DragonFruit.Font = New System.Drawing.Font("Comic Sans MS", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_DragonFruit.Location = New System.Drawing.Point(268, 331)
         Me.lbl_DragonFruit.Name = "lbl_DragonFruit"
-        Me.lbl_DragonFruit.Size = New System.Drawing.Size(86, 31)
+        Me.lbl_DragonFruit.Size = New System.Drawing.Size(89, 38)
         Me.lbl_DragonFruit.TabIndex = 11
         Me.lbl_DragonFruit.Text = "Name"
         '
         'lbl_Banana
         '
         Me.lbl_Banana.AutoSize = True
-        Me.lbl_Banana.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.lbl_Banana.Font = New System.Drawing.Font("Comic Sans MS", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbl_Banana.Location = New System.Drawing.Point(129, 137)
         Me.lbl_Banana.Name = "lbl_Banana"
-        Me.lbl_Banana.Size = New System.Drawing.Size(86, 31)
+        Me.lbl_Banana.Size = New System.Drawing.Size(89, 38)
         Me.lbl_Banana.TabIndex = 10
         Me.lbl_Banana.Text = "Name"
+        '
+        'pbx_Question3
+        '
+        Me.pbx_Question3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pbx_Question3.Location = New System.Drawing.Point(408, 24)
+        Me.pbx_Question3.Name = "pbx_Question3"
+        Me.pbx_Question3.Size = New System.Drawing.Size(110, 110)
+        Me.pbx_Question3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbx_Question3.TabIndex = 8
+        Me.pbx_Question3.TabStop = False
+        '
+        'pbx_Question1
+        '
+        Me.pbx_Question1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pbx_Question1.Location = New System.Drawing.Point(117, 24)
+        Me.pbx_Question1.Name = "pbx_Question1"
+        Me.pbx_Question1.Size = New System.Drawing.Size(110, 110)
+        Me.pbx_Question1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbx_Question1.TabIndex = 7
+        Me.pbx_Question1.TabStop = False
+        '
+        'pbx_Question2
+        '
+        Me.pbx_Question2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pbx_Question2.Location = New System.Drawing.Point(255, 218)
+        Me.pbx_Question2.Name = "pbx_Question2"
+        Me.pbx_Question2.Size = New System.Drawing.Size(110, 110)
+        Me.pbx_Question2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbx_Question2.TabIndex = 6
+        Me.pbx_Question2.TabStop = False
+        '
+        'pbx_Answer3
+        '
+        Me.pbx_Answer3.InitialImage = Nothing
+        Me.pbx_Answer3.Location = New System.Drawing.Point(255, 401)
+        Me.pbx_Answer3.Name = "pbx_Answer3"
+        Me.pbx_Answer3.Size = New System.Drawing.Size(110, 110)
+        Me.pbx_Answer3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbx_Answer3.TabIndex = 2
+        Me.pbx_Answer3.TabStop = False
+        '
+        'pbx_Answer1
+        '
+        Me.pbx_Answer1.InitialImage = Nothing
+        Me.pbx_Answer1.Location = New System.Drawing.Point(23, 401)
+        Me.pbx_Answer1.Name = "pbx_Answer1"
+        Me.pbx_Answer1.Size = New System.Drawing.Size(110, 110)
+        Me.pbx_Answer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbx_Answer1.TabIndex = 1
+        Me.pbx_Answer1.TabStop = False
+        '
+        'pbx_Answer2
+        '
+        Me.pbx_Answer2.InitialImage = Nothing
+        Me.pbx_Answer2.Location = New System.Drawing.Point(139, 401)
+        Me.pbx_Answer2.Name = "pbx_Answer2"
+        Me.pbx_Answer2.Size = New System.Drawing.Size(110, 110)
+        Me.pbx_Answer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbx_Answer2.TabIndex = 0
+        Me.pbx_Answer2.TabStop = False
         '
         'Form1
         '
@@ -186,38 +215,41 @@ Partial Class Form1
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(639, 523)
         Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lbl_ScoreNum)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btn_Check)
-        Me.Controls.Add(Me.pbx_DragonFruit)
-        Me.Controls.Add(Me.pbx_Orange)
-        Me.Controls.Add(Me.pbx_Banana)
+        Me.Controls.Add(Me.pbx_Answer3)
+        Me.Controls.Add(Me.pbx_Answer1)
+        Me.Controls.Add(Me.pbx_Answer2)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.pbx_Banana, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.pbx_Orange, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.pbx_DragonFruit, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.pbx_AnswerOrange, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.pbx_AnswerBanana, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.pbx_AnswerDragonFruit, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        CType(Me.pbx_Trash, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbx_Question3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbx_Question1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbx_Question2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbx_Answer3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbx_Answer1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbx_Answer2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents pbx_Banana As PictureBox
-    Friend WithEvents pbx_Orange As PictureBox
-    Friend WithEvents pbx_DragonFruit As PictureBox
+    Friend WithEvents pbx_Answer2 As PictureBox
+    Friend WithEvents pbx_Answer1 As PictureBox
+    Friend WithEvents pbx_Answer3 As PictureBox
     Friend WithEvents btn_Check As Button
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents pbx_AnswerOrange As PictureBox
-    Friend WithEvents pbx_AnswerBanana As PictureBox
-    Friend WithEvents pbx_AnswerDragonFruit As PictureBox
+    Friend WithEvents lbl_ScoreNum As Label
+    Friend WithEvents pbx_Question3 As PictureBox
+    Friend WithEvents pbx_Question1 As PictureBox
+    Friend WithEvents pbx_Question2 As PictureBox
     Friend WithEvents Panel1 As Panel
     Friend WithEvents lbl_Orange As Label
     Friend WithEvents lbl_DragonFruit As Label
     Friend WithEvents lbl_Banana As Label
+    Friend WithEvents pbx_Trash As PictureBox
+    Friend WithEvents lbl_Reset As Label
 End Class
--- Lab5QuizAppSagabaen/Form1.vb
+++ Lab5QuizAppSagabaen/Form1.vb
@@ -1,35 +1,165 @@
-﻿Public Class Form1
+﻿Imports System.Windows.Forms.VisualStyles
+
+Public Class Form1
     Dim Banana As Fruit
     Dim Orange As Fruit
     Dim DragonFruit As Fruit
     Dim Fruits(3) As Fruit
+    Dim AnswerPictureBoxes() As PictureBox
+    Dim QuestionPictureBoxes() As PictureBox
     Dim Points As Integer
     Dim labels(3) As Label
+    Dim down As Boolean
+    Dim Swap As Boolean = False
+    Dim Answer As Boolean
+    Dim currentDrag As PictureBox
+    Dim Temp As PictureBox
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Banana = New Fruit("Banana", pbx_Banana.Image)
-        Orange = New Fruit("Orange", pbx_Orange.Image)
-        DragonFruit = New Fruit("Dragon Fruit", pbx_DragonFruit.Image)
+        pbx_Question1.AllowDrop = True
+        pbx_Question2.AllowDrop = True
+        pbx_Question3.AllowDrop = True
+        pbx_Trash.AllowDrop = True
+        Banana = New Fruit("Banana", My.Resources.Banana)
+        Orange = New Fruit("Orange", My.Resources.Orange)
+        DragonFruit = New Fruit("Dragon Fruit", My.Resources.DragonFruit)
         Fruits = {Banana, Orange, DragonFruit}
-        labels = {lbl_Banana, lbl_Orange, lbl_DragonFruit}
+        AnswerPictureBoxes = {pbx_Answer1, pbx_Answer2, pbx_Answer3}
+        QuestionPictureBoxes = {pbx_Question1, pbx_Question2, pbx_Question3}
+        labels = {lbl_Banana, lbl_DragonFruit, lbl_Orange}
         For i = 0 To Fruits.Length - 1
             labels(i).Text = Fruits(i).Name
+            AnswerPictureBoxes(i).Image = Fruits(i).Image
         Next
     End Sub
 
     Private Sub btn_Check_Click(sender As Object, e As EventArgs) Handles btn_Check.Click
-        'Dim 
-        'Points = 0
-        'For i = 1 To Fruits.Length
-        '    Compare(Fruits(i).Image, )
-        'Next
+        Points = 0
+        For i = 0 To Fruits.Length - 1
+            Compare(Points, QuestionPictureBoxes(i).Image, Fruits(i).Image)
+        Next
+        lbl_ScoreNum.Text = Points
+        Select Case Points
+            Case 0
+                MessageBox.Show("Try Again Next Time!")
+            Case 1
+                MessageBox.Show("Good!")
+            Case 2
+                MessageBox.Show("Great!")
+            Case 3
+                MessageBox.Show("Excellent!!!")
+        End Select
+    End Sub
+    Sub Compare(ByRef points As Integer, ByRef ImageCompare As Bitmap, ByRef AnswerImage As Bitmap)
+        Try
+            If ImageCompare IsNot Nothing And AnswerImage IsNot Nothing Then
+                If ImageCompare.Equals(AnswerImage) Then
+                    points = points + 1
+                End If
+            End If
+        Catch ex As Exception
+            Return
+        End Try
+    End Sub
+
+    Private Sub pbx_Answers_MouseDown(sender As Object, e As MouseEventArgs) Handles pbx_Answer3.MouseDown, pbx_Answer2.MouseDown, pbx_Answer1.MouseDown
+        Dim Pic As PictureBox = sender
+        currentDrag = Pic
+        Answer = True
+        down = True
+    End Sub
+
+    Private Sub pbx_Answer_MouseMove(sender As Object, e As MouseEventArgs) Handles pbx_Answer3.MouseMove, pbx_Answer2.MouseMove, pbx_Answer1.MouseMove
+        Dim Pic As PictureBox = sender
+        If down And Pic.Image IsNot Nothing Then
+            Pic.DoDragDrop(Pic.Image, DragDropEffects.Move)
+        End If
+        down = False
+    End Sub
+
+    Private Sub pbx_Question_DragDrop(sender As Object, e As DragEventArgs) Handles pbx_Question3.DragDrop, pbx_Question2.DragDrop, pbx_Question1.DragDrop
+        'If Not Answer Then
+        '    Return
+        'End If
+        Dim Pic As PictureBox = sender
+        If Swap Then
+            Dim CurrTemp As PictureBox = currentDrag
+            Pic.Image = CurrTemp.Image
+            currentDrag.Image = Temp.Image
+            'Temp.Image = Pic.Image
+            Swap = False
+            Return
+        Else
+            currentDrag.Image = Nothing
+        End If
+        Pic.Image = e.Data.GetData(DataFormats.Bitmap)
+    End Sub
+
+    Private Sub pbx_Questions_MouseDown(sender As Object, e As MouseEventArgs) Handles pbx_Question3.MouseDown, pbx_Question2.MouseDown, pbx_Question1.MouseDown
+        Dim Pic As PictureBox = sender
+        currentDrag = Pic
+        Answer = False
+        Swap = True
+        down = True
+    End Sub
+
+    Private Sub pbx_Questions_MouseMove(sender As Object, e As MouseEventArgs) Handles pbx_Question3.MouseMove, pbx_Question2.MouseMove, pbx_Question1.MouseMove
+        Dim Pic As PictureBox = sender
+        If down And Pic.Image IsNot Nothing Then
+            Pic.DoDragDrop(Pic.Image, DragDropEffects.Move)
+        End If
+
+        down = False
+    End Sub
+
+    Private Sub pbx_Trash_DragDrop(sender As Object, e As DragEventArgs) Handles pbx_Trash.DragDrop
+        If Not Answer Then
+            currentDrag.Image = Nothing
+        End If
+    End Sub
+
+    Private Sub pbx_All_DragEnter(sender As Object, e As DragEventArgs) Handles pbx_Trash.DragEnter, pbx_Answer3.DragEnter, pbx_Answer2.DragEnter, pbx_Answer1.DragEnter
+
+        If e.Data.GetDataPresent(DataFormats.Bitmap) Then
+            e.Effect = DragDropEffects.Move
+        Else
+            e.Effect = DragDropEffects.None
+        End If
 
     End Sub
-    Function Compare(ByRef points As Integer, ByRef ImageCompare As Bitmap, ByRef AnswerImage As Bitmap) As Boolean
-        If ImageCompare.Equals(AnswerImage) Then
-            points = points + 1
-            Return True
+
+    Private Sub pbx_Trash_Click(sender As Object, e As EventArgs) Handles pbx_Trash.Click
+        For i = 0 To QuestionPictureBoxes.Length - 1
+            QuestionPictureBoxes(i).Image = Nothing
+        Next
+        Points = 0
+        lbl_ScoreNum.Text = Points
+        For i = 0 To Fruits.Length - 1
+            labels(i).Text = Fruits(i).Name
+            AnswerPictureBoxes(i).Image = Fruits(i).Image
+        Next
+    End Sub
+
+    Private Sub pbx_Trash_MouseEnter(sender As Object, e As EventArgs) Handles pbx_Trash.MouseEnter
+        lbl_Reset.Visible = True
+    End Sub
+
+    Private Sub pbx_Trash_MouseLeave(sender As Object, e As EventArgs) Handles pbx_Trash.MouseLeave
+        lbl_Reset.Visible = False
+    End Sub
+
+    Private Sub pbx_Question_DragEnter(sender As Object, e As DragEventArgs) Handles pbx_Question3.DragEnter, pbx_Question2.DragEnter, pbx_Question1.DragEnter
+        Dim Pic As PictureBox = sender
+        If e.Data.GetDataPresent(DataFormats.Bitmap) Then
+            e.Effect = DragDropEffects.Move
         Else
-            Return False
+            e.Effect = DragDropEffects.None
         End If
-    End Function
+    End Sub
+
+    Private Sub pbx_Questions_DragOver(sender As Object, e As DragEventArgs) Handles pbx_Question3.DragOver, pbx_Question2.DragOver, pbx_Question1.DragOver
+        Dim Pic As PictureBox = sender
+        If Swap Then
+            Temp = Pic
+        End If
+    End Sub
 End Class
